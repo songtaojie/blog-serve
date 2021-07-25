10c3fe13d8d90775e404d607d087814e1c8bcd9d
songtaojie <stjworkemail@163.com>
2021-07-25 13:57:30 +0800

    修改配置

--- build/run.sh
+++ build/run.sh
@@ -9,7 +9,7 @@ cd $curpwd
 source ./.env
 # echo -e "\033[33m please input account[$DOCKER_REGISTRY_USERNAME] password,and input e to finish: \033[0m"
 # read input
-docker login --username=$DOCKER_REGISTRY_USERNAME --password=$DOCKER_REGISTRY_PASSWORD $DOCKER_REGISTRY
+# docker login --username=$DOCKER_REGISTRY_USERNAME --password=$DOCKER_REGISTRY_PASSWORD $DOCKER_REGISTRY
 
 
 #±àÒë·þÎñ
