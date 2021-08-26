2f3f2ee5a66ed66f71dc8cd3a5201cdaae24e7c8
songtaojie <stjworkemail@163.com>
2021-08-26 17:44:22 +0800

    修改发布脚本

--- src/HxCore.Web/Controllers/Admin/OperateLogController.cs
+++ src/HxCore.Web/Controllers/Admin/OperateLogController.cs
@@ -3,6 +3,7 @@ using Hx.Sdk.Entity.Page;
 using HxCore.IServices.Admin;
 using HxCore.Model.Admin.OperateLog;
 using HxCore.Web.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,7 @@ namespace HxCore.Web.Controllers.Admin
         /// </summary>
         /// <param name="param">请求参数</param>
         /// <returns></returns>
+        [HttpPost]
         public async Task<PageModel<OperateLogQueryModel>> GetPageAsync(OperateLogQueryParam param)
         {
             return await _service.QueryPageAsync(param);
