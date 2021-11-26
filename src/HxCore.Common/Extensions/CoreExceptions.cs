8dbe2842c84cb9c9e3f49934252833999da55fb8
songtaojie <stjworkemail@163.com>
2021-11-26 22:58:42 +0800

    更新结构

--- src/HxCore.Common/Extensions/CoreExceptions.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace HxCore.Common.Extensions
-{
-}
--- src/HxCore.IRepository/IUserInfoRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using HxCore.Entity.Entities;
-
-namespace HxCore.IRepository
-{
-    public interface IUserInfoRepository:IBaseRepository<T_UserInfo>
-    {
-    }
-}
